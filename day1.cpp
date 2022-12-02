41fba1971ae2b3a6b412bafd7d1499de5d6b37fb
arnavc6 <arnavchokshi172@gmail.com>
2022-12-01 21:38:11 -0500

    day 1 complete

---
+++ day1.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    FILE *file = fopen("/Users/arnavchokshi/Downloads/day1input.txt", "r");
+    char mystring[100];
+    int max = 0;
+    int max2 = 0;
+    int max3 = 0;
+    int holda = 0; 
+    while(fgets(mystring, 100, file)){
+        if (!strcmp(mystring, "\n")) {
+            if(holda > max3){
+                max3 = holda;
+                if(max3 > max2){
+                    int temp = max3;
+                    max3 = max2;
+                    max2 = temp;
+                    if(max2 > max){
+                        int t2 = max2;
+                        max2 = max;
+                        max = t2;
+                    }
+                }
+            }
+            holda = 0;
+        } else {
+            holda += atoi(mystring);
+        }
+    }
+    std::cout << max + max2 + max3 << std::endl;
+}
---
+++ makefile
@@ -0,0 +1,5 @@
+all:
+	g++ day1.cpp 
+	./a.out
+clean:
+	rm a.out
